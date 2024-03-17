fdcebf95cb2a38c01ee9636be615a1f4bafd2a4c
Lawrence-Flash <lawarencetshabalala253@gmail.com>
2024-03-17 06:16:25 -0700

    last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = abs(number) % 10
-if number < 0:
-    digit = -digit
-    print(f"Last digit of {number:d} is {digit:d} and is ", end="")
-if digit > 5:
-    print("greater than 5")
-elif digit == 0:
-    print("0")
+
+# Extracting the last digit of the number using modulus operator
+last_digit = abs(number) % 10
+
+# Checking conditions for the last digit
+if last_digit > 5:
+    print("The string Last digit of", number, "is", last_digit, "and is greater than 5")
+elif last_digit == 0:
+    print("The string Last digit of", number, "is", last_digit, "and is 0")
 else:
-    print("Less than 6 and not 0")
+    print("The string Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
