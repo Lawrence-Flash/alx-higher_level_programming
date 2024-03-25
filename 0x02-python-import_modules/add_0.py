e211a3ce6131f8839d7f8c1290b0c524f4d938c0
Lawrence-Flash <lawarencetshabalala253@gmail.com>
2024-03-24 22:01:04 -0700

    import modules

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """ the addition functio of two numbers
+
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+    the retrun value a + b
+    """
+    return(a + b)
