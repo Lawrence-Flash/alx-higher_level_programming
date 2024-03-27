60d09095ddad6faaa3f7be65f56d56003fe9227c
Lawrence-Flash <lawarencetshabalala253@gmail.com>
2024-03-26 19:56:58 -0700

    updates

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    prev_value = 0
+
+    for ch in roman_string:
+        value = rom_n.get(ch, 0)
+        if value > prev_value:
+            num += value - 2 * prev_value
+        else:
+            num += value
+        prev_value = value
+
+    return num
+
